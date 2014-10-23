ea040a2e3867ec901d621510cf67aa2c24a5c9a1
Sasha Bayan <SashaBayan@gmail.com>
2014-10-23 15:19:14 -0700

    Creates conversion-to-string functionality using for-loop Adds notes for reference

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,17 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+	var keys = Object.keys(obj)
+	console.log(keys)
+	for (var i = 0; i < keys.length; i++) {
+		if(keys[i].constructor === String || Boolean || Number){
+			return keys[i].toString()
+		}
+	};
 };
+
+//Function above successfully converts Object keys, whether String, Boolean, or Number, \
+//to primitive strings. There is redundancy (String does not need to convert to string, I don't think). 
+//Also, this is using a for-loop, instead of recursion.
+
+//keys = obj[i], values = obj[keys[i]
