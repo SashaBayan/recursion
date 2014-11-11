f1ad64c27d8e640dcef9d5b022d7e9e0b0fa8a0d
Sasha Bayan <SashaBayan@gmail.com>
2014-11-10 23:46:32 -0800

    Targets the body element and iterates through its children checking if the className matches what is passed in

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,9 +1,37 @@
-// If life was easy, we could just do things the easy way:
-// var getElementsByClassName = function (className) {
-//   return document.getElementsByClassName(className);
-// };
-
-// But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
-  // your code here
+  var result = [];
+  var children = document.body.childNodes
+	if(document.body.className === className){
+		result.push(document.body)
+	}  
+  for (var i = 0; i < children.length; i++) {
+  	if(children[i].className === className){
+
+  		result.push(children[i])
+  	}
+  };
+
+
+
+	console.log(result);
+return result;
+//  if(hasChildNodes() === true){
+	//check to see if those children have the specified class... aka use recursion
+//}
+//  .className
+//	.classList
+
+//way to check if there's a class name... "class list"
+	//if class list contains target class name, you can push it to an array
+	// use the built-in .contains method that html elements have
+	//iterate through the children of the body
+		//check to see if that child has children... if so, iterate through those #recursion
+	//inner function to search through the children...
+
+//1. chck for body classname
+//2. inner function that takes in a element or node
+
 };
+
+//how do i target the DOM node itself in order to check if it has a class?
+// what DOM nodes am i checking?
