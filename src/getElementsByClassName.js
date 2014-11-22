c637a4bed547e29da92d032ac9f69ff08ea51537
Sasha Bayan <SashaBayan@gmail.com>
2014-11-21 20:35:53 -0800

    Completes getElementsByClassNames using an inner recursive function

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,38 +1,20 @@
 var getElementsByClassName = function(className){
   var result = [];
-  var children = document.body.childNodes
-    if(document.body.className === className){
-	  result.push(document.body)
-	}  
-    for (var i = 0; i < children.length; i++) {
-	  if (children[i].classList !== undefined) {
-	    for (var j = 0; j < children[i].classList.length; j++) {
-  		  if(children[i].classList[j] === className){
-		    result.push(children[i])
-  		  }
-		}
-	  };
-	};
+  function checkNodes(node){
+    var children = node.childNodes;
+  	if(node.classList !== undefined){	
+      for (var i = 0; i < node.classList.length; i++) {
+  	    if(node.classList[i] === className){
+  	  	  result.push(node);
+  		}
+  	  };
+  	}
+  	if(children){
+  	  for (var i = 0; i < children.length; i++) {
+  	    checkNodes(children[i]);
+  	  };
+  	}
+  }
+  checkNodes(document.body)
   return result;
 };
-
-//You should use document.body, element.childNodes, and element.classList
-
-//  if(hasChildNodes() === true){
-	//check to see if those children have the specified class... aka use recursion
-//}
-//  .className
-//	.classList
-
-//way to check if there's a class name... "class list"
-	//if class list contains target class name, you can push it to an array
-	// use the built-in .contains method that html elements have
-	//iterate through the children of the body
-		//check to see if that child has children... if so, iterate through those #recursion
-	//inner function to search through the children...
-
-//1. chck for body classname
-//2. inner function that takes in a element or node
-
-//how do i target the DOM node itself in order to check if it has a class?
-// what DOM nodes am i checking?
